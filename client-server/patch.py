3359f74ab4c203aa3ca13e8f0b289ed6ccb170d0
Pavel Šimerda <psimerda@redhat.com>
2016-02-18 15:40:56 +0100

    patch python-ptrace to use search paths

---
+++ client-server/patch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os
+
+import ptrace.debugger
+
+def patch(ns):
+    def _patch(func):
+        orig = getattr(ns, func.__name__)
+        def new(*args, **kargs):
+            return func(orig, *args, **kargs)
+        setattr(ns, func.__name__, new)
+    return _patch
+
+#
+# Patch python-ptrace
+#
+
+# Change execve/execv to execvpe/execvp so that executed programs
+# are found easily.
+@patch(ptrace.debugger.child)
+def _execChild(orig, arguments, no_stdout, env):
+    if no_stdout:
+        try:
+            null = open(devnull, 'wb')
+            os.dup2(null.fileno(), 1)
+            os.dup2(1, 2)
+            null.close()
+        except IOError as err:
+            os.close(2)
+            os.close(1)
+    try:
+        if env is not None:
+            os.execvpe(arguments[0], arguments, env)
+        else:
+            os.execvp(arguments[0], arguments)
+    except Exception as err:
+        raise ptrace.debugger.child.ChildError(str(err))
