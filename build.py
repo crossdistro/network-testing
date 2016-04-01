55800033d855972d3d4b7a65935521e2991b662f
Petr Viktorin <pviktori@redhat.com>
2016-04-01 16:43:32 +0200

    Build a nice report

--- build.py
+++ build.py
@@ -4,11 +4,13 @@ import os
 import json
 import shutil
 
+import jinja2
 import click
 
 SOURCE_FILES = {
     'schema': 'test-client-server-schema.json',
     'results': 'test-client-server.json',
+    'result_colors': 'result-colors.json',
 }
 
 def load_json(filename):
@@ -27,6 +29,23 @@ def build(datadir, outdir, templatedir):
             for ident, filename in SOURCE_FILES.items()}
 
     print(data)
+
+    loader = jinja2.FileSystemLoader(templatedir)
+    template_env = jinja2.Environment(
+        loader=loader,
+        undefined=jinja2.StrictUndefined,
+        )
+
+    def render(out_name, template_name, **env):
+        template = template_env.get_template(template_name)
+        result = template.render(data=data, **env)
+        with open(os.path.join(outdir, out_name), 'w') as file:
+            file.write(result)
+
+    render('index.html', 'index.html')
+
+    print('Output is in:')
+    print(os.path.abspath(outdir))
 
 
 if __name__ == '__main__':
